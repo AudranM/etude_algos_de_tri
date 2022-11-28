1c61d01223524bc852e41072497f2c0917b0df1f
p2208297 <audran.malosse@etu.univ-lyon1.fr>
2022-11-28 17:41:10 +0100

    ajout de la fonction checktab

---
+++ checktab.py
@@ -0,0 +1,15 @@
+from numpy import *
+from random import *
+def checktab (tab: [int]) -> bool:
+    """
+    pré-condition:
+    post-condition: retourne vrai si le tableau est trié.
+    """
+    i=0
+    longueur=len(tab)
+    while i+1<longueur:
+        if tab[i]>=tab[i+1]:
+            return False
+        else:
+            i+=1
+    return True
