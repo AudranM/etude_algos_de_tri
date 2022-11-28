9aa64921fe6928cf87102baf8315f872e85ef3cd
BERNE LEA p2207775 <lea.berne@etu.univ-lyon1.fr>
2022-11-28 16:22:37 +0000

    Upload New File

---
+++ algo_Z.py
@@ -0,0 +1,32 @@
+from numpy import *
+
+	
+def algo_main(tab : [int]) -> None:
+	"""
+	pré-condition : 
+	post-condition : trie tab par ordre croissant
+	"""
+	i=0
+	while i < len(tab)-1:
+		j=i
+		while j < len(tab)-i-1:
+			if tab[j] >= tab[j+1]:
+				tmp = tab[j]
+				tab[j]=tab[j+1]
+				tab[j+1] = tmp
+			j += 1
+		i += 1
+
+
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
