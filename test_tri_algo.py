3cd7b3af99c154377e5764f9155045f4b2d90ad2
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2023-01-13 21:34:37 +0100

    Ajout du nouveau time_to_sort avec le menu de choix - test trie algo - refactoring du main

--- test_tri_algo.py
+++ test_tri_algo.py
@@ -7,10 +7,12 @@ import checktab as C
 import algo_Z as Z
 import algo_Y as Y
 import algo_X as X
-
-tab1 = R.random_table(1000,1,100)
-print("avant tri : ",C.checktab(tab1))
-print (tab1)
-P.algo_main(tab1)
-print("après tri : ", C.checktab(tab1))
-print (tab1)
+i=0
+while i<100:
+    tab1 = R.random_table(1000,1,100)
+    print("avant tri : ",C.checktab(tab1))
+    print (tab1)
+    P.algo_main(tab1)
+    print("après tri : ", C.checktab(tab1))
+    print (tab1)
+    i+=1
